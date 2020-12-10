360ce1df2c060350e701e9a9b754379b317a181e
edccc <1742648313@qq.com>
2020-12-10 09:44:40 +0800

    在getters中将token作为公共属性供其他页面使用

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,10 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  device: state => state.app.device
+  device: state => state.app.device,
+  // 将token当作公共访问属性导出
+  // token(state) {
+  //   return state.user.token
+  // }
+  token: state => state.user.token
 }
 export default getters
